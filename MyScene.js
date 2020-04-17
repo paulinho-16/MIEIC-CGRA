0844d1418530ca7dd9069c6d31927bd69b12b386
Pedro Ferreira <pdff2000@gmail.com>
2020-04-17 17:02:20 +0100

    Show Cylinder

--- MyScene.js
+++ MyScene.js
@@ -26,9 +26,11 @@ class MyScene extends CGFscene {
         //Initialize scene objects
         this.axis = new CGFaxis(this);
         this.incompleteSphere = new MySphere(this, 16, 8);
+        this.cylinder = new MyCylinder(this, 16, 8);
 
         //Objects connected to MyInterface
         this.displayAxis = true;
+        this.displayCylinder = true;
     }
     initLights() {
         this.lights[0].setPosition(15, 2, 5, 1);
@@ -70,7 +72,9 @@ class MyScene extends CGFscene {
         // ---- BEGIN Primitive drawing section
 
         //This sphere does not have defined texture coordinates
-        this.incompleteSphere.display();
+        //this.incompleteSphere.display();
+        if (this.displayCylinder)
+            this.cylinder.display();
 
         // ---- END Primitive drawing section
     }

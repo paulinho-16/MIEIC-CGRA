a99635c69ec81db26517742db442912a2ce5fe37
Paulinho <up201806505@fe.up.pt>
2020-05-01 19:17:12 +0100

    Added Waggon

--- MyVehicle.js
+++ MyVehicle.js
@@ -7,8 +7,10 @@ class MyVehicle extends CGFobject {
         super(scene);
         this.orientation = 0;
         this.speed = 0;
-        this.position = [0,0,0];
+        this.position = [0,10,0];
         this.body = new MySphere(scene, 16, 8);
+        this.waggonEnd = new MySphere(scene, 16, 8);
+        this.waggonBody = new MyCylinder(scene, 16)
         this.scalef=1.0;
     }
 
@@ -30,7 +32,7 @@ class MyVehicle extends CGFobject {
     reset() {
         this.orientation = 0;
         this.speed = 0;
-        this.position = [0,0,0];
+        this.position = [0,10,0];
     }
 
     scaleVehicle(scaleFactor)
@@ -39,20 +41,44 @@ class MyVehicle extends CGFobject {
     }
 
     display() {
-        this.scene.pushMatrix(); 
 
-        this.scene.scale(2,2,2);
+        this.scene.pushMatrix();
 
         this.scene.translate(this.position[0], this.position[1], this.position[2]);
         this.scene.rotate(this.orientation,0,1,0);
-
         this.scene.scale(this.scalef,this.scalef,this.scalef);
-
-        this.scene.scale(0.5, 0.5, 1);
-        this.scene.translate(0,0,-0.5);
-        this.scene.rotate(Math.PI/2,1,0,0);
-
+        
+        // Display Body
+        this.scene.pushMatrix();
+        this.scene.scale(1,1,2);
+        this.scene.rotate(Math.PI/2,1,0,0);        
         this.body.display();
+        this.scene.popMatrix();
+
+        // Display Waggon
+        this.scene.pushMatrix();
+        this.scene.translate(0, -1.07 , -0.5);
+        this.scene.scale(0.15,0.15,1);
+        this.scene.rotate(Math.PI/2,1,0,0);
+        this.waggonBody.display();
+        this.scene.popMatrix();
+
+        // Display Waggon Front End
+        this.scene.pushMatrix();
+        this.scene.translate(0, -1.07, 0.5);
+        this.scene.scale(0.148, 0.148, 0.148);
+        this.scene.rotate(Math.PI/2,1,0,0);
+        this.waggonEnd.display();
+        this.scene.popMatrix();
+
+        // Display Waggon Back End
+        this.scene.pushMatrix();
+        this.scene.translate(0, -1.07 , -0.5);
+        this.scene.scale(0.148, 0.148, 0.148);
+        this.scene.rotate(Math.PI/2,1,0,0);
+        this.waggonEnd.display();
+        this.scene.popMatrix();
+
         this.scene.popMatrix();
     }
 }

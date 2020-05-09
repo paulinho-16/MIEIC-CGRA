495b9eac1e7ce4fa3bb8ca47bf5b86fd31c80135
Paulinho <up201806505@fe.up.pt>
2020-05-09 17:27:27 +0100

    Added Supply Textures

--- MySupply.js
+++ MySupply.js
@@ -7,6 +7,41 @@ class MySupply extends CGFobject {
 	constructor(scene) {
     super(scene);
     this.quad = new MyQuad(this.scene);
+    initTextures();
+  }
+
+  initTextures() {
+    this.supplyFront = new CGFtexture(scene, 'images/SupplyFront.png');
+    this.supplyTopBottom = new CGFtexture(scene, 'images/SupplyTopBottom.png');
+    this.supplySides = new CGFtexture(scene, 'images/SupplySides.png');
+  }
+
+  initMaterials() {
+    initMaterials(scene) {
+      this.supplyFrontMaterial = new CGFappearance(scene);
+      this.supplyTopBottomMaterial.setAmbient(0.2, 0.4, 0.8, 1.0);
+      this.supplyTopBottomMaterial.setDiffuse(0.2, 0.4, 0.8, 1.0);
+      this.supplyTopBottomMaterial.setSpecular(0.2, 0.4, 0.8, 1.0);
+      this.supplyTopBottomMaterial.setShininess(10.0);
+      this.supplyFrontMaterial.setTexture(this.supplyFront);
+      this.supplyFrontMaterial.setTextureWrap('REPEAT', 'REPEAT');
+  
+      this.supplyTopBottomMaterial = new CGFappearance(scene);
+      this.supplyTopBottomMaterial.setAmbient(0.2, 0.4, 0.8, 1.0);
+      this.supplyTopBottomMaterial.setDiffuse(0.2, 0.4, 0.8, 1.0);
+      this.supplyTopBottomMaterial.setSpecular(0.2, 0.4, 0.8, 1.0);
+      this.supplyTopBottomMaterial.setShininess(10.0);
+      this.supplyTopBottomMaterial.setTexture(this.supplyTopBottom);
+      this.supplyTopBottomMaterial.setTextureWrap('REPEAT', 'REPEAT');
+  
+      this.supplySidesMaterial = new CGFappearance(scene);
+      this.supplySidesMaterial.setAmbient(0.2, 0.4, 0.8, 1.0);
+      this.supplySidesMaterial.setDiffuse(0.2, 0.4, 0.8, 1.0);
+      this.supplySidesMaterial.setSpecular(0.2, 0.4, 0.8, 1.0);
+      this.supplySidesMaterial.setShininess(10.0);
+      this.supplySidesMaterial.setTexture(this.supplySides);
+      this.supplySidesMaterial.setTextureWrap('REPEAT', 'REPEAT');
+    }
   }
 
   display() {
@@ -14,6 +49,7 @@ class MySupply extends CGFobject {
     // Face da Frente
     this.scene.pushMatrix();
     this.scene.translate(0,0,0.5);
+    this.supplyFrontMaterial.apply();
     this.quad.display();
     this.scene.popMatrix();
 
@@ -22,6 +58,7 @@ class MySupply extends CGFobject {
     this.scene.translate(0,0,-0.5);
     this.scene.rotate(Math.PI, 1, 0, 0);
     this.scene.rotate(Math.PI, 0, 0, 1);
+    this.supplySidesMaterial.apply();
     this.quad.display();
     this.scene.popMatrix();
 
@@ -29,6 +66,7 @@ class MySupply extends CGFobject {
     this.scene.pushMatrix();
     this.scene.translate(-0.5,0,0);
     this.scene.rotate(-Math.PI/2.0, 0, 1, 0);
+    this.supplySidesMaterial.apply();
     this.quad.display();
     this.scene.popMatrix();
 
@@ -36,6 +74,7 @@ class MySupply extends CGFobject {
     this.scene.pushMatrix();
     this.scene.translate(0.5,0,0);
     this.scene.rotate(Math.PI/2.0, 0, 1, 0);
+    this.supplySidesMaterial.apply();
     this.quad.display();
     this.scene.popMatrix();
 
@@ -43,6 +82,7 @@ class MySupply extends CGFobject {
     this.scene.pushMatrix();
     this.scene.translate(0,0.5,0);
     this.scene.rotate(-Math.PI/2.0, 1, 0, 0);
+    this.supplyTopBottomMaterial.apply();
     this.quad.display();
     this.scene.popMatrix();
 
@@ -50,6 +90,7 @@ class MySupply extends CGFobject {
     this.scene.pushMatrix();
     this.scene.translate(0,-0.5,0);
     this.scene.rotate(Math.PI/2.0, 1, 0, 0);
+    this.supplyTopBottomMaterial.apply();
     this.quad.display();
     this.scene.popMatrix();
   }
